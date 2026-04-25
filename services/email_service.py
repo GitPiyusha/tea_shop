7bf6f17e7ae3e9a6f876bf11aa648ee2b170aa44
Piyusha Awate <piyushaa424@gmail.com>
2026-04-25 12:31:37 +0530

    Successfully added Email Service

--- services/email_service.py
+++ services/email_service.py
@@ -1,15 +1,15 @@
 import smtplib
-
-
+import os
 
 class EmailServices():
     def send_email(self, message, sender, recipient):
         try:
-            s = smtplib.SMTP('smtp.gmail.com', 587)
+            s = smtplib.SMTP('smtp.gmail.com', 587, timeout=10)
             s.starttls()
-            s.login("EMAIL_ID", "EMAIL_PASSWORD")
+            s.login( os.getenv("EMAIL_ID"), os.getenv("EMAIL_PASSWORD"))
+
             message = "Hello "
-            s.sendmail("EMAIL_ID", recipient, message)
+            s.sendmail(os.getenv("EMAIL_ID"), recipient, message)
             s.quit()
         except Exception as e:
             print(f"Error sending email: {e}")
